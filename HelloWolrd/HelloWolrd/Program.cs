99c98029a1e993a336b2023a5c0c5f526b1c2c10
engrjunaidali <80405587+engrjunaidali@users.noreply.github.com>
2021-03-27 23:25:28 -0700

    Add files via upload

---
+++ HelloWolrd/HelloWolrd/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWolrd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string a;
+            a = "Junaid";
+            Console.WriteLine("Hello " + a);
+        }
+    }
+}
